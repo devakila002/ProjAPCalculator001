0f4a8add11a6e3199b0d9aa333e107dbc7522f37
devakila <dev.akila001@gmail.com>
2023-08-17 12:15:30 +0530

    Added a funcation, Add5number with Branch02

--- MathManger.cs
+++ MathManger.cs
@@ -4,6 +4,7 @@ public class MathManger
     {
         MathManger myCal = new MathManger();
         myCal.Add3Numbers();
+        myCal.Add5Numbers(1,2,3,4,5);
     }
 
     public void Add3Numbers()
@@ -11,8 +12,16 @@ public class MathManger
         int n1 = 5;
         int n2 = 9;
         int sumOfNumbers = n1 + n2 ;
-         System.Console.Write("The Sum of two numbers is : ");
+        System.Console.Write("The Sum of two numbers is : ");
         System.Console.WriteLine(sumOfNumbers);
+    }
+
+    public void Add5Numbers(int a, int b, int c, int d, int e)
+    {
+        
+        int sumOf5Numbers = a+b+c+d+e;
+        System.Console.Write("The Sum of 5 numbers is : ");
+        System.Console.WriteLine(sumOf5Numbers);
     }
 
 }
